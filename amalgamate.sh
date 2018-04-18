3af5c82fd397338a51c129bc19f2e7ee0a0d6f3f
Martin <martin@urbackup.org>
2018-04-18 17:11:16 +0200

    Add license, changelog, readme and example files to release zip

--- amalgamate.sh
+++ amalgamate.sh
@@ -41,9 +41,27 @@ done
 rm test.out
 rm main.c
 
+cp ChangeLog.md amalgamation/
+cp LICENSE amalgamation/
+cp readme.md amalgamation/
+mkdir -p amalgamation/examples
+cp examples/* amalgamation/examples/
+
 cd amalgamation
 ! test -e miniz.zip || rm miniz.zip
-echo -e "miniz.c\nminiz.h" | zip -@ miniz
+cat << EOF | zip -@ miniz
+miniz.c
+miniz.h
+ChangeLog.md
+LICENSE
+readme.md
+examples/example1.c
+examples/example2.c
+examples/example3.c
+examples/example4.c
+examples/example5.c
+examples/example6.c
+EOF
 cd ..
 
 echo "Amalgamation created."
