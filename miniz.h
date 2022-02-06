f2b59b344d6a85822dba64b96d8265c3b4f3070d
Martin <martin@urbackup.org>
2022-02-06 14:39:27 +0100

    Increment major version

--- miniz.h
+++ miniz.h
@@ -1,4 +1,4 @@
-/* miniz.c 2.2.0 - public domain deflate/inflate, zlib-subset, ZIP reading/writing/appending, PNG writing
+/* miniz.c 3.0.0 - public domain deflate/inflate, zlib-subset, ZIP reading/writing/appending, PNG writing
    See "unlicense" statement at the end of this file.
    Rich Geldreich <richgel99@gmail.com>, last updated Oct. 13, 2013
    Implements RFC 1950: http://www.ietf.org/rfc/rfc1950.txt and RFC 1951: http://www.ietf.org/rfc/rfc1951.txt
@@ -272,10 +272,10 @@ enum
     MZ_DEFAULT_COMPRESSION = -1
 };
 
-#define MZ_VERSION "10.2.0"
-#define MZ_VERNUM 0xA100
-#define MZ_VER_MAJOR 10
-#define MZ_VER_MINOR 2
+#define MZ_VERSION "11.0.0"
+#define MZ_VERNUM 0xB000
+#define MZ_VER_MAJOR 11
+#define MZ_VER_MINOR 0
 #define MZ_VER_REVISION 0
 #define MZ_VER_SUBREVISION 0
 
