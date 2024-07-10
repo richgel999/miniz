7e1c3c97c0742e2abcb654145946b2f3efed0f58
LemonBoy <thatlemon@gmail.com>
2024-07-10 13:47:30 +0200

    Set "Made By" in the Central Directory
    Same as it's already done for Zip64.

--- miniz_zip.c
+++ miniz_zip.c
@@ -3164,6 +3164,7 @@ static int mz_stat64(const char *path, struct __stat64 *buffer)
         (void)pZip;
         memset(pDst, 0, MZ_ZIP_CENTRAL_DIR_HEADER_SIZE);
         MZ_WRITE_LE32(pDst + MZ_ZIP_CDH_SIG_OFS, MZ_ZIP_CENTRAL_DIR_HEADER_SIG);
+        MZ_WRITE_LE16(pDst + MZ_ZIP_CDH_VERSION_MADE_BY_OFS, 0x031E); /* TODO: always Unix */
         MZ_WRITE_LE16(pDst + MZ_ZIP_CDH_VERSION_NEEDED_OFS, method ? 20 : 0);
         MZ_WRITE_LE16(pDst + MZ_ZIP_CDH_BIT_FLAG_OFS, bit_flags);
         MZ_WRITE_LE16(pDst + MZ_ZIP_CDH_METHOD_OFS, method);
